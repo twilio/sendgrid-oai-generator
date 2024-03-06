7522bc4cae4629bb1468a8ab3ac48eeb9b92bca1
sbansla <sbansla@twilio.com>
2024-03-06 17:07:25 +0530

    chore: init commit

---
+++ src/main/java/com/sendgrid/oai/SendgridJavaGenerator.java
@@ -0,0 +1,6 @@
+package com.sendgrid.oai;
+public class SendgridJavaGenerator {
+    public static void main(String[] args) {
+        System.out.println("Hello Sendgrid main");
+    }
+}
---
+++ src/test/java/com/sendgrid/oai/SendgridGeneratorTest.java
@@ -0,0 +1,7 @@
+package com.sendgrid.oai;
+
+public class SendgridGeneratorTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Sendgrid test");
+    }
+}

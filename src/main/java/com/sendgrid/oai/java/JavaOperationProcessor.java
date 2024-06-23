1323c9e9366355391c90c082d950ff35820ca0f4
sbansla <sbansla@twilio.com>
2024-06-23 22:58:43 +0530

    chore: fixed enum and list response

--- src/main/java/com/sendgrid/oai/java/JavaOperationProcessor.java
+++ src/main/java/com/sendgrid/oai/java/JavaOperationProcessor.java
@@ -73,27 +73,47 @@ public class JavaOperationProcessor extends OperationProcessor {
         return this;
     }
 
+    /*
+     * Return type needs to be set at 2 places.
+     * 1. While passing in JsonUtil
+     * 2. In Return type.
+     */
     private void setResponseDataType() {
         if (codegenOperation.responses == null || codegenOperation.responses.isEmpty()) return;
         for (CodegenResponse codegenResponse: codegenOperation.responses) {
             if (codegenResponse.is2xx) {
                 if (codegenResponse.dataType == null) {
-                    codegenOperation.vendorExtensions.put(ApplicationConstants.SUCCESS_DATATYPE, "Void");
+                    codegenOperation.vendorExtensions.put(ApplicationConstants.SUCCESS_RETURN_TYPE, "Void");
+                    codegenOperation.vendorExtensions.put(ApplicationConstants.SUCCESS_JSON_TYPE, "Void");
                     codegenOperation.vendorExtensions.put(ApplicationConstants.VOID_DATATYPE, true);
                 } else {
-                    codegenOperation.vendorExtensions.put(ApplicationConstants.SUCCESS_DATATYPE, codegenResponse.dataType);
+                    setSuccessDatatype(codegenResponse);
                 }
             }
             // Set Redirection as Success only when 2xx response not present.
              else if (codegenResponse.is3xx) {
                 if (codegenResponse.dataType == null) {
-                    codegenOperation.vendorExtensions.put(ApplicationConstants.SUCCESS_DATATYPE, "Void");
+                    codegenOperation.vendorExtensions.put(ApplicationConstants.SUCCESS_RETURN_TYPE, "Void");
+                    codegenOperation.vendorExtensions.put(ApplicationConstants.SUCCESS_JSON_TYPE, "Void");
                     codegenOperation.vendorExtensions.put(ApplicationConstants.VOID_DATATYPE, true);
                 } else {
-                    codegenOperation.vendorExtensions.put(ApplicationConstants.SUCCESS_DATATYPE, codegenResponse.dataType);
+                    setSuccessDatatype(codegenResponse);
                 }
             }
         }
+    }
+    
+    public void setSuccessDatatype(CodegenResponse codegenResponse) {
+        String successJsontype = "Void";
+        if (codegenResponse.containerTypeMapped != null) {
+            successJsontype = codegenResponse.containerTypeMapped;
+            codegenOperation.vendorExtensions.put(ApplicationConstants.SUCCESS_LIST_DATATYPE, true);
+        } else {
+            successJsontype = codegenResponse.dataType;
+        }
+        codegenOperation.vendorExtensions.put(ApplicationConstants.SUCCESS_JSON_TYPE, successJsontype);
+        codegenOperation.vendorExtensions.put(ApplicationConstants.SUCCESS_RETURN_TYPE, codegenResponse.dataType);
+        
     }
     
 }
